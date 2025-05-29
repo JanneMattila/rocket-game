517eee77f6baf5e10c9d82d1f291d9da697e6874
Janne Mattila <janne.mattila@hotmail.fi>
2025-05-29 17:31:22 +0300

    Add build ack list

--- test/RocketServerTests/SequenceNumberTests.cpp
+++ test/RocketServerTests/SequenceNumberTests.cpp
@@ -1,4 +1,5 @@
 #include "pch.h"
+#include <vector>
 #include "CppUnitTest.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -23,6 +24,20 @@ namespace RocketServerTests
 				return next - previous;
 			}
 			return next - previous;
+		}
+
+		inline void BuildAckList(const std::vector<uint16_t>& data, const uint16_t& ack, uint32_t& ackBits)
+		{
+			ackBits = 0;
+			for (int i = 1; i <= 32; ++i)
+			{
+				uint16_t seq = static_cast<uint16_t>(ack - i);
+				// Check if seq is in data
+				if (std::find(data.begin(), data.end(), seq) != data.end())
+				{
+					ackBits |= (1u << (32 - i));
+				}
+			}
 		}
 	public:
 		TEST_METHOD(Next_Sequence_Number_Normal1_Test)
@@ -79,6 +94,98 @@ namespace RocketServerTests
 
 			// Assert
 			Assert::AreEqual(expected, actual, L"Validation should have failed");
+		}
+
+		// All 32 previous packets present: ackBits should be all 1s
+		TEST_METHOD(BuildAckList_AllPresent)
+		{
+			uint16_t ack = 100;
+			std::vector<uint16_t> data;
+			for (int i = 1; i <= 32; ++i)
+				data.push_back(ack - i);
+			uint32_t ackBits = 0;
+			BuildAckList(data, ack, ackBits);
+			Assert::AreEqual(0xFFFFFFFFu, ackBits, L"All bits should be set");
+		}
+
+		// No previous packets present: ackBits should be all 0s
+		TEST_METHOD(BuildAckList_NonePresent)
+		{
+			uint16_t ack = 100;
+			std::vector<uint16_t> data; // empty
+			uint32_t ackBits = 0xFFFFFFFF;
+			BuildAckList(data, ack, ackBits);
+			Assert::AreEqual(0u, ackBits, L"No bits should be set");
+		}
+
+		// Some packets missing (holes)
+		TEST_METHOD(BuildAckList_SomeMissing)
+		{
+			uint16_t ack = 100;
+			std::vector<uint16_t> data = {99, 97, 95}; // Only 99, 97, 95 present
+			uint32_t ackBits = 0;
+			BuildAckList(data, ack, ackBits);
+			// 99: bit 31, 97: bit 29, 95: bit 27
+			uint32_t expected = (1u << 31) | (1u << 29) | (1u << 27);
+			Assert::AreEqual(expected, ackBits, L"Only bits for 99, 97, 95 should be set");
+		}
+
+		// Wraparound at 0
+		TEST_METHOD(BuildAckList_Wraparound)
+		{
+			uint16_t ack = 2;
+			std::vector<uint16_t> data = {1, 0, 65535, 65534};
+			uint32_t ackBits = 0;
+			BuildAckList(data, ack, ackBits);
+			// 1: bit 31, 0: bit 30, 65535: bit 29, 65534: bit 28
+			uint32_t expected = (1u << 31) | (1u << 30) | (1u << 29) | (1u << 28);
+			Assert::AreEqual(expected, ackBits, L"Bits for 1, 0, 65535, 65534 should be set");
+		}
+
+		// First and last bit set
+		TEST_METHOD(BuildAckList_FirstAndLastBit)
+		{
+			uint16_t ack = 100;
+			std::vector<uint16_t> data = {99, 68}; // 99: bit 31, 68: bit 0
+			uint32_t ackBits = (1u << 31) | 1u;
+			uint32_t actual = 0;
+			BuildAckList(data, ack, actual);
+			Assert::AreEqual(ackBits, actual, L"Only highest and lowest bits should be set");
+		}
+
+		// Duplicates in data
+		TEST_METHOD(BuildAckList_Duplicates)
+		{
+			uint16_t ack = 100;
+			std::vector<uint16_t> data = {99, 99, 98, 98};
+			uint32_t ackBits = 0;
+			BuildAckList(data, ack, ackBits);
+			// 99: bit 31, 98: bit 30
+			uint32_t expected = (1u << 31) | (1u << 30);
+			Assert::AreEqual(expected, ackBits, L"Bits for 99 and 98 should be set, duplicates ignored");
+		}
+
+		// Random order in data
+		TEST_METHOD(BuildAckList_RandomOrder)
+		{
+			uint16_t ack = 100;
+			std::vector<uint16_t> data = {95, 99, 97};
+			uint32_t ackBits = 0;
+			BuildAckList(data, ack, ackBits);
+			// 99: bit 31, 97: bit 29, 95: bit 27
+			uint32_t expected = (1u << 31) | (1u << 29) | (1u << 27);
+			Assert::AreEqual(expected, ackBits, L"Order in data should not matter");
+		}
+
+		// Only one packet present
+		TEST_METHOD(BuildAckList_OnePresent)
+		{
+			uint16_t ack = 100;
+			std::vector<uint16_t> data = {90}; // 90: bit 9
+			uint32_t ackBits = 0;
+			BuildAckList(data, ack, ackBits);
+			uint32_t expected = (1u << (32 - (100 - 90)));
+			Assert::AreEqual(expected, ackBits, L"Only bit for 90 should be set");
 		}
 	};
 }
